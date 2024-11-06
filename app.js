21f5de70dd3ff9effc2daa1bccec414236127b9f
VictorPrado002 <victor000hugo10@gmail.com>
2024-11-06 11:49:32 -0600

    cors installed

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const routes = require('./routes/routes');
 const app = express();
 const PORT = 3000; 
 const bodyParser = require('body-parser');
-//const cors = require('cors');
+const cors = require('cors');
 
-//app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/',routes);
